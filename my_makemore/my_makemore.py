d113a020ae035d6d46ecf6766655a9d6f05c0c06
myron <myronthk@gmail.com>
2025-03-18 19:32:19 +0100

    understanding nn bigrams

--- my_makemore/my_makemore.py
+++ my_makemore/my_makemore.py
@@ -1,7 +1,9 @@
 #%%
 import torch
 import matplotlib.pyplot as plt
+import torch.nn.functional as F
 %matplotlib inline
+
 #%%
 with open("names.txt", "r") as f:
     data = f.read().splitlines()
@@ -31,6 +33,7 @@ for name in data:
         j = stoi[ch2]
         N[i,j] += 1
 #%%
+############################## NICE PRINT OF THE TABLE ####################################################
 plt.figure(figsize=(16,16))
 plt.imshow(N, cmap='Blues')
 for i in range(27):
@@ -39,22 +42,46 @@ for i in range(27):
         plt.text(j, i, chstr, ha="center", va="bottom", color='gray')
         plt.text(j, i, N[i, j].item(), ha="center", va="top", color='gray')
 plt.axis('off');
-
 #%%
+P = N / torch.sum(N, 1, keepdim=True) # Broadcasting CAREFULL!
+
 g = torch.Generator().manual_seed(2147483647)
 names = []
 for i in range(20):
-    line = N[0, :]
+    p = P[0,:]
     name = ''
     while True:
-        p = line / sum(line).item()
         ix = torch.multinomial(p, num_samples=1, replacement=True, generator=g).item()
         letter = itos[ix]
         if letter == '.':
             break
         name += letter
-        line = N[ix, :]
-    # names.append(name)
+        p = P[stoi[letter], :]
     print(name)
 
+# %%
+########################################### NEURAL NETWORK APPROACH ################################
+# goal --> predicting next letter, based on the previous letter.
+# step 1: isolate 1 name (emma)
+# create all the bigrams 
+# try to complete goal
+xs = []
+ys = []
+
+for name in data:
+    chs = ['.'] + list(name) + ['.']
+    for ch1, ch2 in zip(chs, chs[1:]):
+        xs.append(stoi[ch1])
+        ys.append(stoi[ch2])
+    break
+
+xs = torch.tensor(xs)
+ys = torch.tensor(ys)
+
+xenc = F.one_hot(xs[0], num_classes=27).float()
+yenc = F.one_hot(ys, num_classes=27).float()
+
+W = torch.rand((27,27)) # for 1 neuron
+activation = xenc @ W 
+print(activation.shape)
 # %%
